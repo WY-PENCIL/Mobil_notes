30fd833d4f2ff69ac4989f5964b12f9659753876
王幽 <5116765+pencil-space@user.noreply.gitee.com>
2024-12-24 06:55:23 +0800

    feat : 递归，闭包，装饰器

--- python/params.py
+++ python/params.py
@@ -1,4 +1,12 @@
-a = 2
-b=3
-2*3
-print(_)
+# complex(**{'real': 3, 'imag': 5}) 是什么意思 表示一个复数，一个虚数，其中实部为3，虚部为5
+# a={1,2,3}
+# b={3,4,5}
+# print(a&b)
+# print(a|b)
+
+# b = list({'name':12,'age':30}.items())
+# print(b)
+# __name__ = "params"
+import 斐波那契数列
+
+print("输入模块的名字：",斐波那契数列.__name__)
